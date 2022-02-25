cd5e1656637e4c0bfb70aee10b63372b907116d6
aern1111 <14546@sansai.ac.th>
2022-02-25 19:26:44 +0700

    Lab7

---
+++ Assets/Script/ChangeAnimate.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimate : MonoBehaviour
+{
+    public void getAnimatorDefalt(){
+        this.GetComponent<Animator>().Play("Angry");
+    }
+
+    public void getAnimatorWalk(){
+        this.GetComponent<Animator>().Play("Catwalk Idle To Walk Forward");
+    }
+}
